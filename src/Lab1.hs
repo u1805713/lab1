ccf6471b7c034cb371d638f2af709bec898418f9
Michael Gale <m.gale@warwick.ac.uk>
2017-08-22 14:28:08 +0100

    Initial code commit

--- src/Lab1.hs
+++ src/Lab1.hs
@@ -8,18 +8,47 @@
 
 module Lab1 where
 
+--------------------------------------------------------------------------------
 
+import Prelude hiding (not, and, max)
 
 --------------------------------------------------------------------------------
--- 1.
---------------------------------------------------------------------------------
+
+-- 0. Define `age` to be your age.
 
 age = 26
 
---------------------------------------------------------------------------------
--- 2.
---------------------------------------------------------------------------------
+-- 1. Define `name` to be your name.
+
+name = "Michael"
+
+-- 2. Modify the definition of `triple` so that it triples its argument.
 
 triple = \x -> x * 2
+
+-- 3. Replace `undefined` in the definition of `tripleV2` below so that
+-- it behaves like `triple` above.
+
+tripleV2 x = undefined
+
+-- 4. Replace `undefined` in the definition of `not` below to implement
+-- boolean negation.
+
+not = undefined
+
+-- 5. Replace `undefined` in the definition of `and` below to implement
+-- boolean conjunction.
+
+and = undefined
+
+-- 6. Replace `undefined` in the definition of `max` below so that it reduces
+-- to the greater number of `x` and `y`.
+
+max x y = undefined
+
+-- 7. Replace `undefined` in the definition of `perimeterRect` so that it
+-- calculates the perimeter of a rectangle given its length and width.
+
+perimeterRect l w = undefined
 
 --------------------------------------------------------------------------------
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,8 +3,36 @@
 -- Lab 1: Getting started                                                     --
 --------------------------------------------------------------------------------
 
-module Spec where
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 
+import qualified Lab1 as L
 
+--------------------------------------------------------------------------------
+
+-- | The main entry point to the test suite.
+main :: IO ()
+main = hspec $ do
+    describe "triple" $ do
+        prop "triples numbers" $ \(Positive n) ->
+            let r = L.triple n in r `div` 3 == n && r `mod` 3 == 0
+    describe "tripleV2" $ do
+        prop "triples numbers" $ \(Positive n) ->
+            let r = L.tripleV2 (n :: Int) in r `div` 3 == n && r `mod` 3 == 0
+    describe "not" $ do
+        it "negates booleans" $ L.not True == False && L.not False == True
+    describe "and" $ do
+        it "implements boolean conjunction" $
+            L.and True True == True &&
+            L.and False False == False &&
+            L.and True False == False &&
+            L.and False True == False
+    describe "max" $ do
+        prop "determines the maximum of two numbers" $ \n m ->
+            L.max (n :: Int) (m :: Int) == max n m
+    describe "perimeterRect" $ do
+        prop "calculates the perimeter" $ \(Positive l) (Positive w) ->
+            L.perimeterRect (l :: Int) (w :: Int) == l*2 + w*2
 
 --------------------------------------------------------------------------------
