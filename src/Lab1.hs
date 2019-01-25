c621168aa537c77e774d78321827dc02308dd382
Michael Gale <m.gale@warwick.ac.uk>
2019-01-25 09:49:36 +0000

    Add solutions

--- src/Lab1.hs
+++ src/Lab1.hs
@@ -19,39 +19,68 @@ import Prelude hiding (not, and, max)
 
 -- 0. Define `age` to be your age.
 
+age :: Num a => a
 age = 27
 
 -- 1. Define `name` to be your name.
 
+name :: String
 name = "Michael"
 
 -- 2. Modify the definition of `triple` so that it triples its argument.
 
-triple = \x -> x * 2
+triple :: Num a => a -> a
+triple = \x -> x * 3
 
 -- 3. Replace `undefined` in the definition of `tripleV2` below so that
 -- it behaves like `triple` above.
 
-tripleV2 x = undefined
+tripleV2 :: Num a => a -> a
+tripleV2 x = x * 3
 
 -- 4. Replace `undefined` in the definition of `not` below to implement
 -- boolean negation.
 
-not = undefined
+not :: Bool -> Bool
+not True  = False
+not False = True
+
+not' :: Bool -> Bool
+not' x = case x of
+    True  -> False
+    False -> True
+
+not'' :: Bool -> Bool
+not'' = \x -> case x of
+    True -> False
+    _    -> True
+
+not''' :: Bool -> Bool
+not''' = \x -> case x of
+    True -> False
+    y    -> True
 
 -- 5. Replace `undefined` in the definition of `and` below to implement
 -- boolean conjunction.
 
-and = undefined
+and :: Bool -> Bool -> Bool
+and True True = True
+and _    _    = False
 
 -- 6. Replace `undefined` in the definition of `max` below so that it reduces
 -- to the greater number of `x` and `y`.
 
-max x y = undefined
+max :: Ord a => a -> a -> a
+max x y | x > y     = x
+        | otherwise = y
+
+max' :: Ord a => a -> a -> a
+max' x y = if x > y then x else y
 
 -- 7. Replace `undefined` in the definition of `perimeterRect` so that it
 -- calculates the perimeter of a rectangle given its length and width.
 
-perimeterRect l w = undefined
+perimeterRect :: Num a => a -> a -> a
+perimeterRect l w = 2*l + 2*w
 
 --------------------------------------------------------------------------------
